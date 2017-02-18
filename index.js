ffcc86607dd0702685ebbc949a2fe493a3e39a2d
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-18 21:33:12 +0100

    Adds repack of data

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const readFileSync = require('fs').readFileSync
 const marked = require('marked')
 const { json, send } = require('micro')
+const repackData = require('./lib/repack-data')
 
 module.exports = async (request, response) => {
   if (request.method === 'POST') {
-    const data = await json(request)
-    console.log(data)
-    send(response, 200, {message: 'received', success: true})
+    const data = repackData(await json(request))
+    send(response, 200, {message: 'received', success: true, data: data})
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
