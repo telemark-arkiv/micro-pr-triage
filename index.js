d7c91f4dc1a0e48c6d92b59a70f908a1b3eb556a
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-19 10:52:24 +0100

    Add triaging

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const readFileSync = require('fs').readFileSync
 const marked = require('marked')
 const { json, send } = require('micro')
 const repackData = require('./lib/repack-data')
+const addLabels = require('./lib/add-labels')
+const addAssignes = require('./lib/add-assignes')
 
 module.exports = async (request, response) => {
   if (request.method === 'POST') {
     const data = repackData(await json(request))
-    send(response, 200, {message: 'received', success: true, data: data})
+    const assignees = await addAssignes(data)
+    const labels = await addLabels(data)
+    const result = {
+      action: assignees.action !== 'nothing' && labels.action !== 'nothing',
+      labels: labels,
+      assignees: assignees,
+      data: data
+    }
+    send(response, 200, result)
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
