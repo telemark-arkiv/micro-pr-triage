f9bd578f40c293ba7a79b06f5014382b25bae75b
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-16 10:34:39 +0200

    Moves assignees to config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const assignees = process.env.ASSIGNEES ? process.env.ASSIGNEES.split(',') : false
+
 module.exports = {
   GITHUB_TOKEN: process.env.GITHUB_TOKEN || 'GITHUB_TOKEN',
-  GITHUB_USER: process.env.GITHUB_USER || 'GITHUB_USER'
+  GITHUB_USER: process.env.GITHUB_USER || 'GITHUB_USER',
+  ASSIGNEES: assignees || ['zrrrzzt']
 }
