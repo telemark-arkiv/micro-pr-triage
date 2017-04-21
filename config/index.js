707dabb26f2c26ae5d4304857a5e14cecd755437
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-21 07:29:30 +0200

    Implements signature-validation

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 const assignees = process.env.ASSIGNEES ? process.env.ASSIGNEES.split(',') : false
 
 module.exports = {
+  GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   GITHUB_TOKEN: process.env.GITHUB_TOKEN || 'GITHUB_TOKEN',
   GITHUB_USER: process.env.GITHUB_USER || 'GITHUB_USER',
   ASSIGNEES: assignees || ['zrrrzzt']
--- index.js
+++ index.js
@@ -7,21 +7,35 @@ const repackData = require('./lib/repack-data')
 const addLabels = require('./lib/add-labels')
 const addAssignes = require('./lib/add-assignes')
 const getPkg = require('./lib/get-pkg')
+const validatePayload = require('./lib/validate-payload')
 
 module.exports = async (request, response) => {
   if (request.method === 'POST') {
-    let data = repackData(await json(request))
-    const pkg = await getPkg(data)
-    data.pkg = pkg
-    const assignees = await addAssignes(data)
-    const labels = await addLabels(data)
-    const result = {
-      action: assignees.action !== 'nothing' && labels.action !== 'nothing',
-      labels: labels,
-      assignees: assignees,
-      data: data
+    const headers = request.headers
+    const payload = await json(request)
+    const isValid = validatePayload({headers: headers, body: payload})
+
+    if (isValid === true) {
+      try {
+        let data = repackData(payload)
+        const pkg = await getPkg(data)
+        data.pkg = pkg
+        const assignees = await addAssignes(data)
+        const labels = await addLabels(data)
+        const result = {
+          action: assignees.action !== 'nothing' && labels.action !== 'nothing',
+          labels: labels,
+          assignees: assignees,
+          data: data
+        }
+        send(response, 200, result)
+      } catch (error) {
+        console.error(error)
+        send(response, 500, {success: false, error: error.message})
+      }
+    } else {
+      send(response, 401, 'invalid signature')
     }
-    send(response, 200, result)
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
